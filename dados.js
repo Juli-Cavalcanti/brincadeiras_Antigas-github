5d5a55309f082cdb2a67f321fc74174b71ff9461
Juli-Cavalcanti <152875677+Juli-Cavalcanti@users.noreply.github.com>
2024-09-07 17:30:11 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados = [
+
+    {
+        titulo: "Brincadeira de Roda",
+        descricao: "é uma brincadeira clássica e divertida que marcou a infância de muitas pessoas. É um jogo simples, mas que exige concentração e rapidez",
+        link: "https://pt.wikipedia.org/wiki/Cantiga_de_roda#:~:text=Cantigas%20de%20roda%20(tamb%C3%A9m%20conhecidas,acerca%20da%20letra%20da%20m%C3%BAsica",
+        imagem: "",
+        tags: ""
+    },
+
+    {
+        titulo: "Pique-esconde",
+        descricao: "Uma das brincadeiras mais populares, onde um jogador tenta encontrar os outros que estão escondidos",
+        link:"https://exemplo.com/pique-esconde",
+        imagem: "",
+        tags: ""
+    },
+
+    {
+        titulo:"Queimada",
+        descricao:"Um jogo de equipe onde os jogadores tentam se queimar, acertando os adversários com uma bola",
+        link:"https://exemplo.com/queimada",
+        imagem: "",
+        tags: ""
+    },
+
+    {
+        titulo:"Bolinha de gude",
+        descricao:"Clássico jogo de habilidade e estratégia com pequenas esferas de vidro",
+        link:"https://exemplo.com/bolinha-de-gude",
+        imagem: "",
+        tags: ""
+    },
+
+    {
+        titulo:"Rodar o Pião",
+        descricao:"Uma brincadeira tradicional que exige habilidade e equilíbrio. O objetivo é manter o pião girando por mais tempo",
+        link:"https://exemplo.com/rodar-piao",
+        imagem: "",
+        tags: ""
+    },
+
+    {
+        titulo : "Ioiô",
+        descricao:"Um brinquedo clássico que desafia a coordenação motora e a criatividade. Existem diversas manobras e truques para serem realizados com o ioiô.",
+        link:"https://exemplo.com/ioio",
+        imagem: "",
+        tags: ""
+    },
+       
+];
