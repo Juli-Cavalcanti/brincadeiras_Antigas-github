6cb81b5ca24e32fa5da70120b9452463f1db50e7
Juli-Cavalcanti <152875677+Juli-Cavalcanti@users.noreply.github.com>
2024-09-07 23:17:26 -0300

    Update dados.js
    incluindo codigos faltantes

--- dados.js
+++ dados.js
@@ -2,50 +2,45 @@ let dados = [
 
     {
         titulo: "Brincadeira de Roda",
-        descricao: "é uma brincadeira clássica e divertida que marcou a infância de muitas pessoas. É um jogo simples, mas que exige concentração e rapidez",
+        descricao: "é uma brincadeira clássica e divertida que marcou a infância de muitas pessoas. É um jogo simples, mas que exige concentração e rapidez.  As crianças formam uma roda de mãos dadas e cantam melodias folclóricas, podendo executar ou não coreografias acerca da letra da música",
         link: "https://pt.wikipedia.org/wiki/Cantiga_de_roda#:~:text=Cantigas%20de%20roda%20(tamb%C3%A9m%20conhecidas,acerca%20da%20letra%20da%20m%C3%BAsica",
-        imagem: "",
-        tags: ""
+        tags: "roda cantam musica"
     },
 
     {
         titulo: "Pique-esconde",
-        descricao: "Uma das brincadeiras mais populares, onde um jogador tenta encontrar os outros que estão escondidos",
-        link:"https://exemplo.com/pique-esconde",
-        imagem: "",
+        descricao: "Uma das brincadeiras mais populares, onde um jogador fica com os olhos fechados contando até certo número combinado com os participantes (geralmente 3 pessoas ou mais),, os demais participantes se escondem, e quando ele terminar de contar vai procurar os que estão escondidos",
+        link:"https://pt.wikipedia.org/wiki/Esconde-esconde",
         tags: ""
     },
 
     {
         titulo:"Queimada",
-        descricao:"Um jogo de equipe onde os jogadores tentam se queimar, acertando os adversários com uma bola",
-        link:"https://exemplo.com/queimada",
-        imagem: "",
+        descricao:"Um jogo de equipe onde os jogadores tentam se queimar, acertando os adversários com uma bola, evitando serem atingidos. O objetivo de cada equipe é eliminar todos os membros da equipe adversária, acertando-os com bolas lançadas",
+        link:"https://pt.wikipedia.org/wiki/Queimada_(esporte)",
         tags: ""
     },
 
     {
         titulo:"Bolinha de gude",
-        descricao:"Clássico jogo de habilidade e estratégia com pequenas esferas de vidro",
-        link:"https://exemplo.com/bolinha-de-gude",
-        imagem: "",
-        tags: ""
+        descricao:"Clássico jogo de habilidade e estratégia com pequenas esferas de vidro, onde os jogadores tentam ganhar as bolinhas do oponente",
+        link:"https://pt.wikipedia.org/wiki/Berlinde",
+        tags: "vidro jogadores ganhar "
     },
 
     {
         titulo:"Rodar o Pião",
-        descricao:"Uma brincadeira tradicional que exige habilidade e equilíbrio. O objetivo é manter o pião girando por mais tempo",
-        link:"https://exemplo.com/rodar-piao",
-        imagem: "",
-        tags: ""
+        descricao:"Uma brincadeira tradicional que exige habilidade e equilíbrio. O objetivo é manter o pião girando por mais tempo. A brincadeira de rodar o pião consiste em puxar uma corda enrolada no pião e colocá-lo a rodar no chão. O pião gira em torno do seu próprio eixo, até perder a força e parar",
+        link:"https://pt.wikipedia.org/wiki/Pi%C3%A3o#:~:text=Quando%20o%20pi%C3%A3o%20para%20de,golpes%20ou%20choques%20dos%20outros.",
+        tags: "corda girar equilibrio"
     },
 
     {
-        titulo : "Ioiô",
+        titulo : "Ioio",
         descricao:"Um brinquedo clássico que desafia a coordenação motora e a criatividade. Existem diversas manobras e truques para serem realizados com o ioiô.",
-        link:"https://exemplo.com/ioio",
+        link:"https://pt.wikipedia.org/wiki/Ioi%C3%B4",
         imagem: "",
-        tags: ""
+        tags: "coordenação manobras truqes"
     },
        
 ];
